082b4e55702b41ed274f2ec7712f56cf6e659ae1
Syam Sundar K <k.syamsundar21@gmail.com>
2019-07-09 09:36:31 +0530

    Shell to extract classes from `.jar` added

---
+++ server/src/shell/pextractor.sh
@@ -0,0 +1,50 @@
+unzip core.jar
+ls processing/core | while read CLASS; do
+    if [[ $CLASS =~ "$" ]];
+	then
+	echo "$CLASS eliminated"
+	else
+	echo $CLASS >> core.txt
+	fi
+done
+ls processing/awt | while read CLASS; do
+    if [[ $CLASS =~ "$" ]];
+    then
+    echo "$CLASS eliminated"
+    else
+    echo $CLASS >> awt.txt
+    fi
+done
+ls processing/data | while read CLASS; do
+    if [[ $CLASS =~ "$" ]];
+    then
+    echo "$CLASS eliminated"
+    else
+    echo $CLASS >> data.txt
+    fi
+done
+ls processing/event | while read CLASS; do
+    if [[ $CLASS =~ "$" ]];
+    then
+    echo "$CLASS eliminated"
+    else
+    echo $CLASS >> event.txt
+fi
+done
+ls processing/javafx | while read CLASS; do
+    if [[ $CLASS =~ "$" ]];
+    then
+    echo "$CLASS eliminated"
+    else
+    echo $CLASS >> javafx.txt
+fi
+done
+ls processing/opengl | while read CLASS; do
+    if [[ $CLASS =~ "$" ]];
+    then
+    echo "$CLASS eliminated"
+    else
+    echo $CLASS >> opengl.txt
+    fi
+done
+exit 0
