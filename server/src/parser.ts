738859bf9f16d40f97e2425708b55c3f063abf96
Syam Sundar K <k.syamsundar21@gmail.com>
2019-07-13 18:41:51 +0530

    whole Tree extraction added

--- server/src/parser.ts
+++ server/src/parser.ts
@@ -1,27 +1,43 @@
 import { parse } from 'java-ast'
 import { ParseTree } from 'antlr4ts/tree/ParseTree'
 
-let mainParserArray: ParseTree[] = new Array();
-let _mainCounter = -1
+let tokenArray: ParseTree[] = new Array();
+let _tokenCounter = -1
+
+let wholeAST: ParseTree[] = new Array();
+let _wholeCounter = -1
 
 export function parseAST(processedText: string) {
 	let ast = parse(processedText)
-	mainParserArray = []
-	_mainCounter = -1
-	var parserTrees: ParseTree[] = new Array();
+	tokenArray = []
+	_tokenCounter = -1
+	wholeAST = []
+	_wholeCounter = -1
 	for(let i = 0; i < ast.childCount; i++){
-		parserTrees[i] = ast.children![i]
-		extractTokens(parserTrees[i])
+		extractTokens(ast.children![i])
 	}
-	console.log(parserTrees)
+	for(let i = 0; i < ast.childCount; i++){
+		wholeASTExtract(ast.children![i])
+	}
+	
+	console.log("parserTrees")
 }
 
 function extractTokens(gotOne: ParseTree){
 	for(let j = 0; j < gotOne.childCount; j++){
 		if(gotOne.getChild(j).childCount == 0){
-			_mainCounter +=1
-			mainParserArray[_mainCounter] = gotOne.getChild(j)
+			_tokenCounter +=1
+			tokenArray[_tokenCounter] = gotOne.getChild(j)
 		}
 		extractTokens(gotOne.getChild(j))
+	}
+}
+
+// top -> bottom ; right -> left
+function wholeASTExtract(gotOne: ParseTree){
+	_wholeCounter += 1
+	wholeAST[_wholeCounter] = gotOne
+	for(let j=0;j<gotOne.childCount;j++){
+		wholeASTExtract(gotOne.getChild(j))
 	}
 }
