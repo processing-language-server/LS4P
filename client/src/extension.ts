5f29645b3847e7e8ef032e8ff105975ad785160d
Syam Sundar K <k.syamsundar21@gmail.com>
2019-08-03 10:07:12 +0530

    Client compilation fix

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -48,7 +48,7 @@ export function activate(context: ExtensionContext) {
 		window.showInformationMessage(`Running Processing Sketch.!`);
 		try{
 			// exec(`mkdir client/out/class`)
-			childProcess.exec(`cp ${__dirname.substring(0,__dirname.length-11)}/server/out/compile/ProcessingDefault.class ${__dirname}/class`)
+			childProcess.exec(`cp ${__dirname.substring(0,__dirname.length-11)}/server/out/compile/** ${__dirname}/class`)
 			childProcess.exec(`cd ${__dirname.substring(0,__dirname.length-11)}/client/out/class ; java ProcessingDefault`)
 		} catch(e){
 			window.showInformationMessage(`Error occured while running sketch.!`);
--- server/src/scripts/syslogs.ts
+++ server/src/scripts/syslogs.ts
@@ -2,5 +2,5 @@ const exec = require('child_process').exec;
 
 export function writeLog(logContents: String) {
 	let datetime = new Date()
-	exec(`echo \"${datetime} -> ${logContents}\" >> ${__dirname.substring(0,__dirname.length-8)}/logs/syslogs.txt`)
+	exec(`echo \"${datetime.toISOString().slice(0,22)} -> ${logContents}\" >> ${__dirname.substring(0,__dirname.length-8)}/logs/syslogs.txt`)
 }
